9543f2791a67e78fb64192c64342452d2c1783bb
John Wood <johna613@gmail.com>
2023-04-27 12:50:22 -0400

    setup

--- app.py
+++ app.py
@@ -1,1 +1,26 @@
- 
+from flask import Flask, redirect, render_template, url_for, request, flash, session, g
+from flask_debugtoolbar import DebugToolbarExtension
+import json
+from keys import REC_API_KEY, MAPS_KEY, TOMTOM_KEY
+
+from sqlalchemy.exc import IntegrityError
+
+from models import connect_db, db
+
+app = Flask(__name__)
+app.app_context().push()
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///rec_trips'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+app.config['SECRET_KEY'] = "secrets"
+
+debug = DebugToolbarExtension(app)
+
+connect_db(app)
+
+db.create_all()
+
+REC_BASE_URL = "https://ridb.recreation.gov/api/v1"
+GEOCODE_BASE_URL = f"https://api.tomtom.com/search/2/geocode/"
---
+++ models.py
@@ -0,0 +1,9 @@
+from flask_bcrypt import Bcrypt
+from flask_sqlalchemy import SQLAlchemy
+
+bcrypt = Bcrypt()
+db = SQLAlchemy()
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
