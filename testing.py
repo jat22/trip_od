3be0167c43629172379667d390f0c1ffefb1f635
John Wood <johna613@gmail.com>
2023-07-02 10:57:28 -0400

    landing template

---
+++ testing.py
@@ -0,0 +1,464 @@
+results = {
+	"summary": {
+		"query": "portland or",
+		"queryType": "NON_NEAR",
+		"queryTime": 31,
+		"numResults": 10,
+		"offset": 0,
+		"totalResults": 32,
+		"fuzzyLevel": 1
+	},
+	"results": [
+		{
+			"type": "Geography",
+			"id": "n_aM6A8jltSGHiYmo5kRZw",
+			"score": 4.8443818092,
+			"entityType": "Municipality",
+			"address": {
+				"municipality": "Portland",
+				"countrySubdivision": "OR",
+				"countrySubdivisionName": "Oregon",
+				"countryCode": "US",
+				"country": "United States",
+				"countryCodeISO3": "USA",
+				"freeformAddress": "Portland, OR"
+			},
+			"position": {
+				"lat": 45.51602,
+				"lon": -122.68143
+			},
+			"viewport": {
+				"topLeftPoint": {
+					"lat": 45.85833,
+					"lon": -122.91998
+				},
+				"btmRightPoint": {
+					"lat": 45.37951,
+					"lon": -122.43867
+				}
+			},
+			"boundingBox": {
+				"topLeftPoint": {
+					"lat": 45.85833,
+					"lon": -122.91998
+				},
+				"btmRightPoint": {
+					"lat": 45.37951,
+					"lon": -122.43867
+				}
+			},
+			"dataSources": {
+				"geometry": {
+					"id": "c25e6504-01e8-4682-b90c-163dc0e7e0d2"
+				}
+			}
+		},
+		{
+			"type": "Geography",
+			"id": "CX5UYmL6KGM5Y_vzC38rgQ",
+			"score": 3.7952156067,
+			"entityType": "Neighbourhood",
+			"address": {
+				"municipalitySubdivision": "Downtown Portland",
+				"countrySecondarySubdivision": "Multnomah",
+				"countrySubdivision": "OR",
+				"countrySubdivisionName": "Oregon",
+				"countryCode": "US",
+				"country": "United States",
+				"countryCodeISO3": "USA",
+				"freeformAddress": "Downtown Portland, OR"
+			},
+			"position": {
+				"lat": 45.51521,
+				"lon": -122.67838
+			},
+			"viewport": {
+				"topLeftPoint": {
+					"lat": 45.5231,
+					"lon": -122.68954
+				},
+				"btmRightPoint": {
+					"lat": 45.50539,
+					"lon": -122.66902
+				}
+			},
+			"boundingBox": {
+				"topLeftPoint": {
+					"lat": 45.5231,
+					"lon": -122.68954
+				},
+				"btmRightPoint": {
+					"lat": 45.50539,
+					"lon": -122.66902
+				}
+			},
+			"dataSources": {
+				"geometry": {
+					"id": "0000554f-5200-3c00-0000-000023c34673"
+				}
+			}
+		},
+		{
+			"type": "Geography",
+			"id": "-a9Ib4MkdfLtzBvUtyTuhg",
+			"score": 3.7952156067,
+			"entityType": "Neighbourhood",
+			"address": {
+				"municipalitySubdivision": "South Portland",
+				"countrySecondarySubdivision": "Multnomah",
+				"countrySubdivision": "OR",
+				"countrySubdivisionName": "Oregon",
+				"countryCode": "US",
+				"country": "United States",
+				"countryCodeISO3": "USA",
+				"freeformAddress": "South Portland, OR"
+			},
+			"position": {
+				"lat": 45.48379,
+				"lon": -122.6758
+			},
+			"viewport": {
+				"topLeftPoint": {
+					"lat": 45.5074,
+					"lon": -122.68278
+				},
+				"btmRightPoint": {
+					"lat": 45.46446,
+					"lon": -122.66522
+				}
+			},
+			"boundingBox": {
+				"topLeftPoint": {
+					"lat": 45.5074,
+					"lon": -122.68278
+				},
+				"btmRightPoint": {
+					"lat": 45.46446,
+					"lon": -122.66522
+				}
+			},
+			"dataSources": {
+				"geometry": {
+					"id": "0000554f-5200-3c00-0000-000023c3465c"
+				}
+			}
+		},
+		{
+			"type": "Geography",
+			"id": "pbfnIFE5GqZlyFoC8KGufA",
+			"score": 3.7748348713,
+			"entityType": "Neighbourhood",
+			"address": {
+				"municipalitySubdivision": "West Portland Park",
+				"countrySecondarySubdivision": "Multnomah",
+				"countrySubdivision": "OR",
+				"countrySubdivisionName": "Oregon",
+				"countryCode": "US",
+				"country": "United States",
+				"countryCodeISO3": "USA",
+				"freeformAddress": "West Portland Park, OR"
+			},
+			"position": {
+				"lat": 45.44928,
+				"lon": -122.72048
+			},
+			"viewport": {
+				"topLeftPoint": {
+					"lat": 45.45708,
+					"lon": -122.7333
+				},
+				"btmRightPoint": {
+					"lat": 45.44016,
+					"lon": -122.71038
+				}
+			},
+			"boundingBox": {
+				"topLeftPoint": {
+					"lat": 45.45708,
+					"lon": -122.7333
+				},
+				"btmRightPoint": {
+					"lat": 45.44016,
+					"lon": -122.71038
+				}
+			},
+			"dataSources": {
+				"geometry": {
+					"id": "0000554f-5200-3c00-0000-000023c3465e"
+				}
+			}
+		},
+		{
+			"type": "Geography",
+			"id": "pX4xD8yx_gz7breA2lgL2A",
+			"score": 2.4272780418,
+			"entityType": "Municipality",
+			"address": {
+				"municipality": "Portland",
+				"countrySecondarySubdivision": "Cumberland",
+				"countrySubdivision": "ME",
+				"countrySubdivisionName": "Maine",
+				"countryCode": "US",
+				"country": "United States",
+				"countryCodeISO3": "USA",
+				"freeformAddress": "Portland, ME"
+			},
+			"position": {
+				"lat": 43.65922,
+				"lon": -70.25652
+			},
+			"viewport": {
+				"topLeftPoint": {
+					"lat": 43.72775,
+					"lon": -70.34731
+				},
+				"btmRightPoint": {
+					"lat": 43.54365,
+					"lon": -69.97581
+				}
+			},
+			"boundingBox": {
+				"topLeftPoint": {
+					"lat": 43.72775,
+					"lon": -70.34731
+				},
+				"btmRightPoint": {
+					"lat": 43.54365,
+					"lon": -69.97581
+				}
+			},
+			"dataSources": {
+				"geometry": {
+					"id": "0000554d-4500-3c00-0000-000059691bb2"
+				}
+			}
+		},
+		{
+			"type": "Geography",
+			"id": "VLM_pFP-iv77OqrnOwfotg",
+			"score": 2.3999698162,
+			"entityType": "Municipality",
+			"address": {
+				"municipality": "Portland",
+				"countrySubdivision": "TN",
+				"countrySubdivisionName": "Tennessee",
+				"countryCode": "US",
+				"country": "United States",
+				"countryCodeISO3": "USA",
+				"freeformAddress": "Portland, TN"
+			},
+			"position": {
+				"lat": 36.5845,
+				"lon": -86.51806
+			},
+			"viewport": {
+				"topLeftPoint": {
+					"lat": 36.65249,
+					"lon": -86.68323
+				},
+				"btmRightPoint": {
+					"lat": 36.47033,
+					"lon": -86.3755
+				}
+			},
+			"boundingBox": {
+				"topLeftPoint": {
+					"lat": 36.65249,
+					"lon": -86.68323
+				},
+				"btmRightPoint": {
+					"lat": 36.47033,
+					"lon": -86.3755
+				}
+			},
+			"dataSources": {
+				"geometry": {
+					"id": "c6f904c1-dbf3-48f6-bc38-e168ae07b95f"
+				}
+			}
+		},
+		{
+			"type": "Geography",
+			"id": "XyQMBWz2siEfNvt-r2mBDA",
+			"score": 2.3920431137,
+			"entityType": "Municipality",
+			"address": {
+				"municipality": "Portland",
+				"countrySubdivision": "TX",
+				"countrySubdivisionName": "Texas",
+				"countryCode": "US",
+				"country": "United States",
+				"countryCodeISO3": "USA",
+				"freeformAddress": "Portland, TX"
+			},
+			"position": {
+				"lat": 27.87671,
+				"lon": -97.32743
+			},
+			"viewport": {
+				"topLeftPoint": {
+					"lat": 27.98227,
+					"lon": -97.44237
+				},
+				"btmRightPoint": {
+					"lat": 27.85006,
+					"lon": -97.22711
+				}
+			},
+			"boundingBox": {
+				"topLeftPoint": {
+					"lat": 27.98227,
+					"lon": -97.44237
+				},
+				"btmRightPoint": {
+					"lat": 27.85006,
+					"lon": -97.22711
+				}
+			},
+			"dataSources": {
+				"geometry": {
+					"id": "86c8c813-7658-41a1-825a-4844b67f2b9a"
+				}
+			}
+		},
+		{
+			"type": "Geography",
+			"id": "O1SX6VwMJyjh3D24gXNXNw",
+			"score": 2.3888852596,
+			"entityType": "Municipality",
+			"address": {
+				"municipality": "Portland",
+				"countrySecondarySubdivision": "Lower Connecticut River Valley",
+				"countrySubdivision": "CT",
+				"countrySubdivisionName": "Connecticut",
+				"countryCode": "US",
+				"country": "United States",
+				"countryCodeISO3": "USA",
+				"freeformAddress": "Portland, CT"
+			},
+			"position": {
+				"lat": 41.58134,
+				"lon": -72.63411
+			},
+			"viewport": {
+				"topLeftPoint": {
+					"lat": 41.64279,
+					"lon": -72.64898
+				},
+				"btmRightPoint": {
+					"lat": 41.55397,
+					"lon": -72.5291
+				}
+			},
+			"boundingBox": {
+				"topLeftPoint": {
+					"lat": 41.64279,
+					"lon": -72.64898
+				},
+				"btmRightPoint": {
+					"lat": 41.55397,
+					"lon": -72.5291
+				}
+			},
+			"dataSources": {
+				"geometry": {
+					"id": "00005858-5800-1200-0000-00007d2c526c"
+				}
+			}
+		},
+		{
+			"type": "Geography",
+			"id": "C7JeuXFYrFdfRnQKvmIAyg",
+			"score": 2.3872439861,
+			"entityType": "Municipality",
+			"address": {
+				"municipality": "Portland",
+				"countrySecondarySubdivision": "Jay",
+				"countrySubdivision": "IN",
+				"countrySubdivisionName": "Indiana",
+				"countryCode": "US",
+				"country": "United States",
+				"countryCodeISO3": "USA",
+				"freeformAddress": "Portland, IN"
+			},
+			"position": {
+				"lat": 40.43234,
+				"lon": -84.97782
+			},
+			"viewport": {
+				"topLeftPoint": {
+					"lat": 40.46165,
+					"lon": -85.00992
+				},
+				"btmRightPoint": {
+					"lat": 40.41233,
+					"lon": -84.95955
+				}
+			},
+			"boundingBox": {
+				"topLeftPoint": {
+					"lat": 40.46165,
+					"lon": -85.00992
+				},
+				"btmRightPoint": {
+					"lat": 40.41233,
+					"lon": -84.95955
+				}
+			},
+			"dataSources": {
+				"geometry": {
+					"id": "00005549-4e00-3c00-0000-000059696f55"
+				}
+			}
+		},
+		{
+			"type": "Geography",
+			"id": "JYrOSQiVGLV0FK1SiUMlDg",
+			"score": 2.3865938187,
+			"entityType": "Municipality",
+			"address": {
+				"municipality": "Portland",
+				"countrySecondarySubdivision": "Chautauqua",
+				"countrySubdivision": "NY",
+				"countrySubdivisionName": "New York",
+				"countryCode": "US",
+				"country": "United States",
+				"countryCodeISO3": "USA",
+				"freeformAddress": "Portland, NY"
+			},
+			"position": {
+				"lat": 42.37984,
+				"lon": -79.46734
+			},
+			"viewport": {
+				"topLeftPoint": {
+					"lat": 42.45417,
+					"lon": -79.52814
+				},
+				"btmRightPoint": {
+					"lat": 42.29881,
+					"lon": -79.41133
+				}
+			},
+			"boundingBox": {
+				"topLeftPoint": {
+					"lat": 42.45417,
+					"lon": -79.52814
+				},
+				"btmRightPoint": {
+					"lat": 42.29881,
+					"lon": -79.41133
+				}
+			},
+			"dataSources": {
+				"geometry": {
+					"id": "00005858-5800-1200-0000-00007d2c623e"
+				}
+			}
+		}
+	]
+}
+
+for result in results["results"]:
+	if result["entityType"] == "Municipality":
+		print(result["address"])
