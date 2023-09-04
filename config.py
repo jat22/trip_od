08cd53adc86221394e25572769a6d662e62cac06
John Wood <johna613@gmail.com>
2023-09-04 17:25:31 -0400

    config

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ load_dotenv()
 
 SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
 SECRET_KEY = os.getenv("SECRET_KEY")
-BASE_URL = os.getenv("BASE_URL")
+
 WTF_CSRF_SECRET_KEY = os.getenv("WTF_CRSF_SECRET_KEY")
